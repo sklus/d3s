dd217e3f9e7c45c054cec50b0ec5a80bdae6d3c4
Stefan Klus <stefan.klus@fu-berlin.de>
2019-04-12 10:02:34 +0200

    Updated setup.py.

--- d3s/algorithms.py
+++ d3s/algorithms.py
@@ -5,20 +5,6 @@ import scipy.sparse.linalg
 import d3s.observables as _observables
 import d3s.kernels as _kernels
 
-# The algorithms are implemented based on the following publications:
-# S. Brunton, J. Proctor, J. Kutz:
-#    Discovering governing equations from data by sparse identification of nonlinear dynamical systems.
-# M. Williams, I. Kevrekidis, C. Rowley:
-#    A data-driven approximation of the Koopman operator: Extending dynamic mode decomposition.
-# M. Williams, C. Rowley, I. Kevrekidis:
-#    A kernel-based method for data-driven Koopman spectral analysis.
-# S. Klus, P. Koltai, C. Schütte:
-#    On the numerical approximation of the Perron-Frobenius and Koopman operator.
-# S. Klus, F. Nüske, P. Koltai, H. Wu, I. Kevrekidis, C. Schütte, F. Noé:
-#    Data-driven model reduction and transfer operator approximation.
-# S. Klus, I. Schuster, K. Muandet:
-#    Eigendecompositions of transfer operators in reproducing kernel Hilbert spaces.
-
 
 def dmd(X, Y, mode='exact'):
     '''
@@ -39,6 +25,7 @@ def dmd(X, Y, mode='exact'):
         raise ValueError('Only exact and standard DMD available.')
 
     return d, Phi
+
 
 def dmdc(X, Y, U, svThresh=1e-10):
     """
--- setup.py
+++ setup.py
@@ -6,23 +6,26 @@ import sys
 
 boost = os.getenv("CONDA_PREFIX", '')
 if not boost:
-    print("\n\033[1;31m WARNING: Not in conda environment. Systems extension may not work correctly\033[0m\n")
+    print("\n\033[1;31m WARNING: Not in conda environment. Systems extension may not work correctly.\033[0m\n")
 
-# create dspy directory. If this does not exist, doing "setup.py install develop" will fail to sym-link.
-dspy_folder_path = os.path.join(os.path.dirname(__file__), "dspy")
-if "develop" in sys.argv and not os.path.exists(dspy_folder_path):
-    os.mkdir(dspy_folder_path)
+# Create d3s directory. If this does not exist, doing "setup.py install develop" will fail to sym-link.
+d3s_folder_path = os.path.join(os.path.dirname(__file__), "d3s")
+if "develop" in sys.argv and not os.path.exists(d3s_folder_path):
+    os.mkdir(d3s_folder_path)
+    
+link_args = '-Wl,-rpath,' + boost + '/lib'
 
-e = Extension('dspy.systems',
+e = Extension('d3s.systems',
               sources=['cpp/systems.cpp'],
               language='c++',
               extra_compile_args=['-c', '-O3', '-fPIC', '-D_UNIX', '-std=c++11', '-Wno-deprecated-declarations'],
+              extra_link_args=[link_args],
               include_dirs=[boost, 'm'],
-              libraries=['boost_python', 'boost_numpy'],
+              libraries=['boost_python37', 'boost_numpy37'],
               )
 
 
-setup(name='dspy',
+setup(name='d3s',
       version='0.1',
       ext_modules=[e],
       packages=find_packages(),
